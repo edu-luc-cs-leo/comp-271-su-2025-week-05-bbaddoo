a46751808aea68fe3fac34cb2ad5ffe24c60cc22
bbaddoo <bbaddoo@luc.edu>
2025-07-02 21:56:23 +0000

    05 finished

--- DynamicArray.java
+++ DynamicArray.java
@@ -63,33 +63,110 @@ public class DynamicArray {
      *         first occurrence of string.
      */
     public int indexOf(String string) {
-        return -1234;
-    } // method indexOf
+        int indexFound = -1;
+    //go through all the elements unless the end or string is found
+    for (int i = 0; i < this.occupancy && indexFound == -1; i++) {
+        //check to see the element is not null or equals the string
+        if (this.underlying[i] != null && this.underlying[i].equals(string)) {
+            indexFound = i; //store the index where it was found 
+        }
+    }
+
+    return indexFound; //return the index
+} // method indexOf
 
     /** Method to tell if a string exists in the underlying array */
     public boolean contains(String string) {
-        return false;
+        boolean found = false;  //start with false (no sting found)
+   //go through all the elements unless the end or string is found
+    for (int i = 0; i < this.occupancy && !found; i++) {
+        //check to see the element is not null or equals the string
+        if (this.underlying[i] != null && this.underlying[i].equals(string)) {
+            found = true; //if found then it's true 
+        }
     }
 
+    return found; //return the found string 
+}
     /** Method to count how many times a string appears in the underlying array*/
     public int countOf(String string) {
-        return -1234;
+        int count = 0; // start with zero matches
+    //loop through all elements of the array 
+    for (int i = 0; i < this.occupancy; i++) {
+        //check to see if the element is not null and goes with the string
+        if (this.underlying[i] != null && this.underlying[i].equals(string)) {
+            count++; //when it matches the count will go up
+        }
     }
+
+    return count; //return the new count
+}
 
     /** method to remove items from the underlying array */
     public String remove(int index) {
-        return "James on the street talking trash about your hotel";
+        // chceck to see if the index is valid (less than 0) if it is return null
+        if (index < 0 || index >= this.occupancy) {
+            return null;
+        
+        }
+        //store the element for now 
+    String removed  = this.underlying[index];
+    //moce all the elements after the one taken out to the left 1
+    for (int i = index; i < this.occupancy - 1; i++) {
+        this.underlying[i] = this.underlying[i+1];
+    }
+    //get rid of the last element 
+    this.underlying[this.occupancy - 1] = null;
+    this.occupancy--;  //lessen the count as one element was taken out
+
+    return removed;  //return removed 
     }
 
     /** overload remove */ 
     public String remove(String string) {
-        return "James on the street talking trash about your hotel";
+      String removed = null;  //no element has been removed yet
+
+   //go through the array until either the end is reached or an element is removed
+    for (int i = 0; i < this.occupancy && removed == null; i++) {
+        //check if current element is not null and equals the string
+        if (this.underlying[i] != null && this.underlying[i].equals(string)) {
+            removed = this.underlying[i]; //store the element 
+
+            // move elements left to fill it in(love left by one spot)
+            for (int j = i; j < this.occupancy - 1; j++) {
+                this.underlying[j] = this.underlying[j + 1];
+            }
+
+            // get rid of the last element 
+            this.underlying[this.occupancy - 1] = null;
+            this.occupancy--; //lessen the count as one element was taken out
+        }
     }
 
+    return removed; //return the removed element 
+}
     /** Complete this method */
     public String toString() {
-        return "to be done shortly"; // blatant violation of magic values clause
-                                     // serves as reminder to finish this method
+        //check if there is no elements in the array
+        if (this.occupancy == 0) {
+        return "DynamicArray is empty"; //message saying that the array is empty if it is
+    }
+//the beginning of the result 
+    String result = "[";
+//go through and loop through all the elements in the array
+    for (int i = 0; i < this.occupancy; i++) {
+        result = result + this.underlying[i];  //append the current element to the result string
+
+        if (i < this.occupancy - 1) {//if this is not the last element then append a comma and space 
+            result = result + ", ";
+        }
+    }
+     //append the closing bracket 
+    result = result + "]";
+    return result; //return the result 
+}
+
+
     }
 
-} // class DynamicArray
+// class DynamicArray
---
+++ reflection.md
@@ -0,0 +1,4 @@
+This assignment I think I am able to better understand a little bit about what you wanted. Looking at the specific methods I see that there are easier/cleaner ways I could have gone about doing the assignment. For example, the CompareTo method yours was very simple and straightforward and mine was not. It is methods like those where I need to do more studying and practice how to make my code more clean instead of messy and kind of all over the place. That is something for future assignments I will work on. It is very helpful on the other hand to see your answers to each part and an explanation on why you did what you did.
+
+
+
